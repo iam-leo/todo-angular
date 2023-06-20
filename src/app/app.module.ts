fde5280fc6d5ea08e75278dcd4ca948d4cceea85
Javier Leonel Sosa <leo.vm.cba@gmail.com>
2023-06-20 20:52:50 -0300

    add function to save a task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { TareasComponent } from './components/tareas/tareas.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { TareasComponent } from './components/tareas/tareas.component';
     TareasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/tareas/tareas.component.ts
+++ src/app/components/tareas/tareas.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from 'src/app/models/Task';
 
 @Component({
   selector: 'app-tareas',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./tareas.component.css']
 })
 export class TareasComponent {
+  listTask: Task[] = [];
+
+  nameTask = '';
+
+  addTask(){
+    //Crear obj de tarea
+    const task: Task = {
+      name: this.nameTask,
+      status: false
+    }
+
+    //Agregar la tarea al array
+    this.listTask.push(task);
+
+    //Reset input
+    this.nameTask = ''
+  }
 
 }
